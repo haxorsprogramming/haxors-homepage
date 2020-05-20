76e8184fed1ca1cd3e7fb9879da165291c5112bf
haxorsprogramming <haxorsuinsu@gmail.com>
2020-05-21 02:31:59 +0700

    moo land jam mila

--- index.php
+++ index.php
@@ -316,7 +316,7 @@ $namaFig = substr($acak, 0,1);
       <div class="container">
         <div class="row mb-5">
           <div class="col-12 text-center">
-            <h2 class="section-title mb-3">Blog Posts</h2>
+            <h2 class="section-title mb-3">Haxors Update</h2>
           </div>
         </div>
 
@@ -324,7 +324,7 @@ $namaFig = substr($acak, 0,1);
           <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
             <div class="h-entry">
               <img src="images/img_1.jpg" alt="Image" class="img-fluid">
-              <h2><a href="#">Create interactive prototypes</a></h2>
+              <h2><a href="#">Cara membuat RestAPI dengan Python</a></h2>
               <div class="meta mb-4">Ham Brook <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
               <p><a href="#">Continue Reading...</a></p>
@@ -333,7 +333,7 @@ $namaFig = substr($acak, 0,1);
           <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
             <div class="h-entry">
               <img src="images/img_2.jpg" alt="Image" class="img-fluid">
-              <h2><a href="#">Create interactive prototypes</a></h2>
+              <h2><a href="#">Tips mengoptimalkan network balance pada socket.io</a></h2>
               <div class="meta mb-4">James Phelps <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
               <p><a href="#">Continue Reading...</a></p>
--- js/haxors.js
+++ js/haxors.js
@@ -1,21 +1,27 @@
 var home = new Vue({
-    el : '#home-section',
-    data : {
-        titleClub : 'Haxorsprogramming',
-        menuHeader : [
-            {teks : 'Home', link :'#home-section'},
-            {teks : 'Project', link : '#project-section'},
-            {teks : 'About Us', link : '#about-section'},
-            {teks : 'Blog', link : 'http://update.haxors.or.id'},
-            {teks : 'Contact', link : '#contact-section'}
+    el: '#home-section',
+    data: {
+        titleClub: 'Haxorsprogramming',
+        menuHeader: [
+            { teks: 'Home', link: '#home-section' },
+            { teks: 'Project', link: '#project-section' },
+            { teks: 'About Us', link: '#about-section' },
+            { teks: 'Blog', link: 'http://update.haxors.or.id' },
+            { teks: 'Contact', link: '#contact-section' }
         ],
-        kegiatan : [
-            {teks : 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom', judul : 'Dilo Hackathon 2018', pic : 'images/kegiatan/dilo_hackaton_2018.jpg'},
-            {teks : 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom', judul : 'Dilo Hackathon 2018', pic : 'images/kegiatan/dilo_hackaton_2018.jpg'},
-            {teks : 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom', judul : 'Dilo Hackathon 2018', pic : 'images/kegiatan/dilo_hackaton_2018.jpg'},
-            {teks : 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom', judul : 'Dilo Hackathon 2018', pic : 'images/kegiatan/dilo_hackaton_2018.jpg'},
-            {teks : 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom', judul : 'Dilo Hackathon 2018', pic : 'images/kegiatan/dilo_hackaton_2018.jpg'},
-            {teks : 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom', judul : 'Dilo Hackathon 2018', pic : 'images/kegiatan/dilo_hackaton_2018.jpg'}
+        kegiatan: [
+            { teks: 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom',
+             judul: 'Dilo Hackathon 2018', pic: 'images/kegiatan/dilo_hackaton_2018.jpg' },
+            { teks: 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom', 
+            judul: 'Dilo Hackathon 2018', pic: 'images/kegiatan/ui_ux_classroom_1.jpg' },
+            { teks: 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom', 
+            judul: 'Dilo Hackathon 2018', pic: 'images/kegiatan/ui_ux_classroom_2.jpg' },
+            { teks: 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom',
+             judul: 'Dilo Hackathon 2018', pic: 'images/kegiatan/dilo_hackaton_2018.jpg' },
+            { teks: 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom',
+             judul: 'Dilo Hackathon 2018', pic: 'images/kegiatan/dilo_hackaton_2018.jpg' },
+            { teks: 'Tim Haxorsprogramming mengirimkan delegasi untuk mengikuti kompetisi hackathon yang diselenggarakan oleh telkom',
+             judul: 'Dilo Hackathon 2018', pic: 'images/kegiatan/dilo_hackaton_2018.jpg' }
 
         ]
     }
